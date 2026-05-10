xgb data leakage fixed

src/python_scripts/training/train_xgb.py
@@ -14,7 +14,7 @@ from optuna.integration import XGBoostPruningCallback
 
 from src.python_scripts.datasets.dataset_xgb import load_pt_samples
 
-def objective(trial, dtrain, dtest, Y_test_clean, seq_len):
+def objective(trial, ds_train, ds_val, ds_test, Y_test_clean, seq_len):
 
     # --- HYPERPARAMETER SPACE ---
     params = {
@@ -40,30 +40,30 @@ def objective(trial, dtrain, dtest, Y_test_clean, seq_len):
 
     run = wandb.init(
         project='eeg-bis-prediction',
-        group=f'xgb-seq-{seq_len}-tuning-1',
+        group=f'xgb-seq-{seq_len}-v1',
         name=f'trial_{trial.number}',
         config=params,
         reinit=True # Allows multiple runs in the same script
     )
     wandb_callback = WandbCallback()
-    pruning_callback = XGBoostPruningCallback(trial, "eval-rmse")
+    pruning_callback = XGBoostPruningCallback(trial, "val-rmse")
+
+    evals_list = [(ds_train, 'train'), (ds_val, 'val')]
 
     # --- TRAINING ---
-    evals = [(dtrain, 'train'), (dtest, 'eval')]
-
     xgb_model = xgb.train(
             params=params, 
-            dtrain=dtrain, 
+            dtrain=ds_train, 
             num_boost_round=600, 
-            evals=evals, 
+            evals=evals_list,
             early_stopping_rounds=20,
             verbose_eval=0, # messages are turned off
             callbacks=[wandb_callback, pruning_callback] 
         )
     
     # Evaluate the best iteration
-    Y_test_flat = Y_test_clean.flatten() # so it matches predictions shape ( (367597) instead of (367597, 1))
-    predictions = xgb_model.predict(dtest)
+    Y_test_flat = Y_test_clean.flatten() # match predictions shape ( (367597) instead of (367597, 1))
+    predictions = xgb_model.predict(ds_test)
     mae = mean_absolute_error(Y_test_flat, predictions)
     mse = mean_squared_error(Y_test_flat, predictions)
     rmse = root_mean_squared_error(Y_test_flat, predictions)
@@ -104,46 +104,53 @@ def main():
     INPUT_DIR = 'data/processed/eeg'
     CASES_FILE = 'data/processed/cases_data.csv'
     SEQ_LEN = 120
+    TRAINING_SIZE = 0.7 # split into 70%/15%/15%
 
     # --- INDEX LOADING ---
     cases_master = pd.read_csv(CASES_FILE)
     all_ids = cases_master['caseid'].tolist()
 
-    train_ids, test_ids = train_test_split(all_ids, test_size=0.2, random_state=2026)
-
+    train_ids, val_test_ids = train_test_split(all_ids, test_size=1-TRAINING_SIZE, random_state=2026)
+    val_ids, test_ids = train_test_split(val_test_ids, test_size=0.5, random_state=2026)
 
     # --- DATA LOADING ---
     # seq_len = trial.suggest_categorical('seq_len', [1, 30, 60, 90, 120])
 
     X_train_3d, Y_train = load_pt_samples(INPUT_DIR, train_ids, SEQ_LEN)
-
+    X_val_3d, Y_val = load_pt_samples(INPUT_DIR, val_ids, SEQ_LEN)
     X_test_3d, Y_test = load_pt_samples(INPUT_DIR, test_ids, SEQ_LEN)
 
     # simply reshaping the data yields better results
     X_train_2d = X_train_3d.reshape(X_train_3d.shape[0], -1)
+    X_val_2d = X_val_3d.reshape(X_val_3d.shape[0], -1)
     X_test_2d = X_test_3d.reshape(X_test_3d.shape[0], -1)
 
     # Free up RAM
-    del X_train_3d, X_test_3d
+    del X_train_3d, X_val_3d, X_test_3d, train_ids, val_test_ids, val_ids, test_ids
     gc.collect()
 
     # CLEAN TARGET VARIABLES (Remove NaNs from Labels)
     valid_train_mask = ~np.isnan(Y_train).flatten()
+    valid_val_mask = ~np.isnan(Y_val).flatten()
     valid_test_mask  = ~np.isnan(Y_test).flatten()
 
     X_train_clean = X_train_2d[valid_train_mask]
     Y_train_clean = Y_train[valid_train_mask]
 
+    X_val_clean = X_val_2d[valid_val_mask]
+    Y_val_clean = Y_val[valid_val_mask]
+
     X_test_clean = X_test_2d[valid_test_mask]
     Y_test_clean = Y_test[valid_test_mask]
 
-    del X_train_2d, X_test_2d, valid_train_mask, valid_test_mask
+    del X_train_2d, X_val_2d, X_test_2d, valid_train_mask, valid_val_mask, valid_test_mask
     gc.collect()
 
     print(f"Clean Training Matrix Shape: {X_train_clean.shape}")
     # Prepare XGBoost Data Structures
-    dtrain = xgb.DMatrix(X_train_clean, label=Y_train_clean)
-    dtest  = xgb.DMatrix(X_test_clean,  label=Y_test_clean)
+    ds_train = xgb.DMatrix(X_train_clean, label=Y_train_clean)
+    ds_val = xgb.DMatrix(X_val_clean, label=Y_val_clean)
+    ds_test  = xgb.DMatrix(X_test_clean,  label=Y_test_clean)
 
     # --- OPTUNA OPTIMIZATION ---
     print("\nStarting Hyperparameter Tuning...")
@@ -151,7 +158,7 @@ def main():
     study = optuna.create_study(direction="minimize")
 
     study.optimize(
-        lambda trial: objective(trial, dtrain, dtest, Y_test_clean, SEQ_LEN),
+        lambda trial: objective(trial, ds_train, ds_val, ds_test, Y_test_clean, SEQ_LEN),
         n_trials=50,
     )
 